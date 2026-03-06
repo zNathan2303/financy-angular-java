dd466c13e05356ecaa083c10b3461f6b35d68ea2
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2026-03-05 23:39:30 -0300

    feat: faltando terminar os filtros

--- src/app/features/transactions/transactions.ts
+++ src/app/features/transactions/transactions.ts
@@ -5,10 +5,11 @@ import { InputBase } from '../../shared/components/inputs/input-base/input-base'
 import { FormControl } from '@angular/forms';
 import { SelectInput } from '../../shared/components/inputs/select-input/select-input';
 import { SelectOption } from '../../shared/components/inputs/select-input/select-option/select-option';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-transactions',
-  imports: [Header, LucideAngularModule, InputBase, SelectInput, SelectOption],
+  imports: [Header, LucideAngularModule, InputBase, SelectInput, SelectOption, NgClass],
   templateUrl: './transactions.html',
   styleUrl: './transactions.css',
 })
@@ -19,4 +20,42 @@ export class Transactions {
   readonly CircleArrowUp = CircleArrowUp;
 
   searchFormControl = new FormControl({ value: '', disabled: false });
+
+  categories = [
+    {
+      id: 1,
+      title: 'Alimentação',
+      color: 'blue',
+    },
+    {
+      id: 2,
+      title: 'Transporte',
+      color: 'purple',
+    },
+    {
+      id: 3,
+      title: 'Mercado',
+      color: 'orange',
+    },
+    {
+      id: 4,
+      title: 'Investimento',
+      color: 'green',
+    },
+    {
+      id: 5,
+      title: 'Utilidades',
+      color: 'yellow',
+    },
+    {
+      id: 6,
+      title: 'Salário',
+      color: 'green',
+    },
+    {
+      id: 7,
+      title: 'Entretenimento',
+      color: 'pink',
+    },
+  ];
 }
