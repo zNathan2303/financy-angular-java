bde05f6b9524dd191e79a075dc6080c6d04580c8
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2026-03-19 23:04:46 -0300

    docs: adiciona campo para o bearer token no swagger

--- backend/src/main/java/br/dev/nathan/financy/config/SwaggerConfiguration.java
+++ backend/src/main/java/br/dev/nathan/financy/config/SwaggerConfiguration.java
@@ -1,20 +1,40 @@
 package br.dev.nathan.financy.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
 
+    final String securitySchemeName = "BearerToken";
+
+    Info info = new Info()
+        .title("Financy API")
+        .version("1.0")
+        .description("Financy application API documentation");
+
+    SecurityScheme securityScheme = new SecurityScheme()
+        .name(securitySchemeName)
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT");
+
+    Components components = new Components()
+        .addSecuritySchemes(securitySchemeName, securityScheme);
+
+    SecurityRequirement securityRequirement = new SecurityRequirement()
+        .addList(securitySchemeName);
+
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-            .title("Financy API")
-            .version("1.0")
-            .description("Documentação da API da aplicação Financy")
-        );
+        return new OpenAPI()
+            .info(info)
+            .components(components)
+            .addSecurityItem(securityRequirement);
     }
-
 }
