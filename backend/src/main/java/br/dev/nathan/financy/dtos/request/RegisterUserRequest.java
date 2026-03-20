7c8393906f5ecf50b9324ec2e0481483728d4e3c
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2026-03-19 21:56:17 -0300

    fix: corrige o RegisterUserRequest deixar passar os valores dos campos em qualquer tamanho

--- backend/src/main/java/br/dev/nathan/financy/dtos/request/RegisterUserRequest.java
+++ backend/src/main/java/br/dev/nathan/financy/dtos/request/RegisterUserRequest.java
@@ -1,15 +1,19 @@
 package br.dev.nathan.financy.dtos.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public record RegisterUserRequest(
 
     @NotEmpty(message = "Name is required.")
+    @Size(max = 50, message = "The maximum name size is 50 characters.")
     String name,
 
     @NotEmpty(message = "Email is required.")
+    @Size(max = 300, message = "The maximum email size is 300 characters.")
     String email,
 
     @NotEmpty(message = "A password is required.")
+    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters.")
     String password
 ) {}
