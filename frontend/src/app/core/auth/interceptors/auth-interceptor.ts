11d63e74cef412575ebf98b944d9cb116c8c52bb
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2026-03-31 20:38:42 -0300

    fix: corrige bug de loading infinito na troca de ambiente entre prod e dev

--- frontend/src/app/core/auth/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/auth/interceptors/auth-interceptor.ts
@@ -1,16 +1,29 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { Auth } from '../services/auth';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(Auth);
   const token = authService.getToken();
+  const router = inject(Router);
 
   if (token) {
     const cloned = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` },
     });
-    return next(cloned);
+    return next(cloned).pipe(
+      catchError((error) => {
+        if (error.status === 401 || (error.status === 403 && !req.url.includes('/auth/login'))) {
+          authService.logout();
+
+          router.navigateByUrl('/login');
+        }
+
+        return throwError(() => error);
+      }),
+    );
   }
 
   return next(req);
